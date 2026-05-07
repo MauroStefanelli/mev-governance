811b084af72b211f74e2da50a7dd422c38266911
Mauro <38442890+MauroStefanelli@users.noreply.github.com>
2026-05-07 12:58:47 +0200

    fix: rimuovi import cors da index.js

--- mev-governance-ui/src/index.js
+++ mev-governance-ui/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import express from "express";
-import cors from "cors";
-
-
-// const app = express();
-
-//app.use(cors());          // ✅ FIX IMMEDIATO
-// app.use(express.json());
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,7 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
