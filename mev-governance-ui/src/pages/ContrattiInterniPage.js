aa1dd59668c2a7e54953400a36f1a53073f7090d
Mauro <38442890+MauroStefanelli@users.noreply.github.com>
2026-06-10 17:17:44 +0200

    AA

--- mev-governance-ui/src/pages/ContrattiInterniPage.js
+++ mev-governance-ui/src/pages/ContrattiInterniPage.js
@@ -225,7 +225,7 @@ function ContrattiPage({ onUnauthorized }) {
                                                      borderBottom: "1px solid #f0f0f0",
                                                    }}>
                                                      <td style={TD("left", { fontSize: "12px" })}>{g.goTo}</td>
-                                                     <td style={TD("left", { fontSize: "12px" })}>{g.applicativo}</td>
+                                                     <td style={TD("left", { fontSize: "12px" })}>CICCIO</td>
                                                      <td style={TD("left", { fontSize: "12px" })}>{g.annoCompetenza}</td>
                                                      <td style={TD("left", { fontSize: "12px" })}>{g.releaseExcel}</td>
                                                      <td style={TD("right", { fontSize: "12px" })}>{formatEuro(g.importoForniturascontato)}</td>
