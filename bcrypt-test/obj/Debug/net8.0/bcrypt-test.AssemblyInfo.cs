4edd52096b23b4583dd333b6c31a096acdc5fb6c
Mauro <38442890+MauroStefanelli@users.noreply.github.com>
2026-07-22 14:54:00 +0200

    push

--- bcrypt-test/obj/Debug/net8.0/bcrypt-test.AssemblyInfo.cs
+++ bcrypt-test/obj/Debug/net8.0/bcrypt-test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bcrypt-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2efe25a6542b518ce177d5af7f52968196bd9200")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9dd20fca819d0eba0801069f520996c8eb1b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("bcrypt-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bcrypt-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
